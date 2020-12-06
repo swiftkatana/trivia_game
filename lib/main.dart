fd950b2a6d335b3bcfc09053dc77c0eeb7add9b0
Daniel Levy <58981441+swiftkatana@users.noreply.github.com>
2020-12-06 21:54:14 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:trivia_game/screens/WellComeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: WellComeScreen(),
+    );
+  }
+}
---
+++ lib/screens/WellComeScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class WellComeScreen extends StatefulWidget {
+  @override
+  _WellComeScreenState createState() => _WellComeScreenState();
+}
+
+class _WellComeScreenState extends State<WellComeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Wellcome");
+  }
+}
