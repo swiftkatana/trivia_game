f1f43826eeef833451c8973cf72c904e6d7a97f4
Daniel Levy <58981441+swiftkatana@users.noreply.github.com>
2020-12-06 22:28:01 +0200

    start working on the willcome page

--- lib/screens/WellComeScreen.dart
+++ lib/screens/WellComeScreen.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:trivia_game/components/TextInput.dart';
 
-class WellComeScreen extends StatefulWidget {
-  @override
-  _WellComeScreenState createState() => _WellComeScreenState();
-}
-
-class _WellComeScreenState extends State<WellComeScreen> {
+class WellComeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Wellcome");
+    return Scaffold(
+        appBar: AppBar(title: Text("Hello world")),
+        body: Align(
+            alignment: Alignment.center,
+            child: Padding(padding: EdgeInsets.all(10), child: Body())));
+  }
+}
+
+// body: Column(children: <Widget>[
+//   Text('Hello world', style: TextStyle(fontSize: 50)),
+//   Expanded(child: Body()),
+// ]),
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  void onCkick() {
+    // Navigator.push(context,
+    // MaterialPageRoute(builder: (context) => MyHomePage(controller.text)));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Wellcome To The Best Trivia',
+          style: TextStyle(
+            fontSize: 25,
+          )),
+      Padding(padding: EdgeInsets.all(40), child: TextInput(onCkick)),
+    ]);
   }
 }
